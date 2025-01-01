63274ebd6febe8eafe49e4861b1d50f6a25dc974
mcmcdat <78667400+mcmcdat@users.noreply.github.com>
2025-01-01 08:59:29 -0500

    go check

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-# Should take in arguments for
-# 1) whether to delete old files or not (default do not),
-# 2) path to songs
-# 3) recursive convert or not
-
 import os
 import sys
 
 from utils import converter
 
 path = sys.argv[1]
-
 delete = (sys.argv[2].lower()=='true')
 recursive = (sys.argv[3].lower()=='true')
 
-converter.convert_to_mp3(sys.argv[1],
-                         delete_original=delete,
-                         recursive=recursive)
+if os.path.exists(path):
+    print(f"Converting files from {path}, where delete = {delete} and recursive = {recursive}.")
+    go = input(f"Proceed? (y/n):")
+    if ((go.lower() == "yes") | (go.lower() == "y")):
+        converter.convert_to_mp3(sys.argv[1],
+                                delete_original=delete,
+                                recursive=recursive)
+    else:
+        print("Task killed")
+else:
+    print(f"Path '{path}' not found!")
